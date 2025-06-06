2ffd342b8ae27995cf459731988b278aeba99a78
kindadailybren <bomedina1@up.edu.ph>
2025-06-07 00:01:23 +0800

    feat: added navbar

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from "@fortawesome/free-solid-svg-icons"
+
+export const Navbar = () => {
+  return (
+    <>
+      <div className="border-b-4 border-[#3A3A3A] h-23 pt-[10px]">
+        <div className="flex gap-2">
+          <FontAwesomeIcon className="mt-[20px] mx-[20px]" icon={faBars} size="2xl" style={{color: "#ffffff",}} />
+          <img src="/studysurf_final.png" className="w-17 h-15"></img>
+          <h1 className="text-[#82B6E0] text-[48px] font-[Kantumruy] ">StudySurf</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
