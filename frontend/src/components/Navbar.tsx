48351926b621f21053b5a85097da26c1a3e84295
not-sol <castones.sol@gmail.com>
2025-06-10 01:46:41 +0800

    feat: opening side bar on hover, tab highlighting

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
       <nav className="border-b-1 border-[var(--primary-border)] h-18 flex gap-2 items-center">
         <FontAwesomeIcon className="my-auto mx-[20px]" icon={faBars} size="2xl" style={{color: "#ffffff",}} />
         <img src="/studysurf_final.png" className="h-10"></img>
-        <h1 className="text-[#82B6E0] text-4xl font-[Kantumruy] ">StudySurf</h1>
+        <h1 className="text-[var(--highlight-text)] text-4xl font-[Kantumruy] ">StudySurf</h1>
       </nav>
     </>
   )
--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,23 +1,27 @@
-
 import '../App.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGear, faCloudArrowUp, faPhotoFilm } from "@fortawesome/free-solid-svg-icons"
+import { useState } from "react";
 
 export const Sidebar = () => {
+  const [activeTab, setActiveTab] = useState("upload");
+
   return (
     <>
-      <div className="flex flex-col gap-y-2 border-r-1 border-[var(--primary-border)] w-60 h-auto pt-5 px-3">
-        <div className="flex items-center gap-x-5 px-4 py-3 bg-[var(--highlight-bg)] rounded-xl text-[var(--highlight-text)]">
-          <FontAwesomeIcon icon={faCloudArrowUp} size="2xl"/>
-          <h1 className="text-center">Upload</h1>
+      <div className="group flex flex-col gap-y-2 border-r border-[var(--primary-border)] w-21.5 h-auto pt-5 px-2 overflow-hidden hover:w-50 transition-all duration-200">
+        <div onClick={() => setActiveTab("upload")} className={`flex items-center gap-x-5 px-3 py-3 rounded-xl cursor-pointer transition duration-200 ease-in-out ${activeTab === "upload" ? "bg-[var(--highlight-bg)] text-[var(--highlight-text)]" : "hover:bg-[var(--highlight-bg)] hover:text-[var(--highlight-text)]"}`}>
+          <FontAwesomeIcon icon={faCloudArrowUp} size="2xl" />
+          <h1 className="text-center opacity-0 group-hover:opacity-100">Upload</h1>
         </div>
-        <div className="flex items-center gap-x-5 px-4 py-3 transition duration-200 ease-in-out hover:bg-[var(--highlight-bg)] rounded-xl hover:text-[var(--highlight-text)] cursor-pointer">
-          <FontAwesomeIcon icon={faPhotoFilm} size="2xl"/>
-          <h1 className="">Gallery</h1>
+        <div onClick={() => setActiveTab("gallery")} className={`flex items-center gap-x-5 px-3 py-3 rounded-xl cursor-pointer transition duration-200 ease-in-out ${activeTab === "gallery"
+              ? "bg-[var(--highlight-bg)] text-[var(--highlight-text)]" : "hover:bg-[var(--highlight-bg)] hover:text-[var(--highlight-text)]"}`}>
+          <FontAwesomeIcon icon={faPhotoFilm} size="2xl" />
+          <h1 className="text-center opacity-0 group-hover:opacity-100">Gallery</h1>
         </div>
-        <div className="flex items-center gap-x-5 px-4 py-3 transition duration-200 ease-in-out hover:bg-[var(--highlight-bg)] rounded-xl hover:text-[var(--highlight-text)] cursor-pointer">
-          <FontAwesomeIcon icon={faGear} size="2xl"/>
-          <h1 className="">Settings</h1>
+        <div onClick={() => setActiveTab("settings")}
+          className={`flex items-center gap-x-5 px-3 py-3 pl-4 rounded-xl cursor-pointer transition duration-200 ease-in-out ${activeTab === "settings" ? "bg-[var(--highlight-bg)] text-[var(--highlight-text)]" : "hover:bg-[var(--highlight-bg)] hover:text-[var(--highlight-text)]"}`}>
+          <FontAwesomeIcon icon={faGear} size="2xl" />
+          <h1 className="text-center opacity-0 group-hover:opacity-100">Settings</h1>
         </div>
       </div>
     </>
