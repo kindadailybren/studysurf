4fa356a37b8127911d8a3a6a9c4d0a2cb72e7c0d
not-sol <castones.sol@gmail.com>
2025-06-11 15:07:11 +0800

    style: small tweaks on side bar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -8,24 +8,24 @@ export const Sidebar = () => {
 
   return (
     <>
-      <div className="group flex flex-col gap-y-2 border-r border-[var(--primary-border)] w-18.5 h-auto pt-5 px-2 overflow-hidden hover:w-40 transition-all duration-200">
-        <div onClick={() => setActiveTab("upload")} className={`grid grid-cols-[30px_1fr] px-3 py-3 rounded-xl cursor-pointer transition duration-200 ease-in-out ${activeTab === "upload" ? "bg-[var(--highlight-bg)] text-[var(--highlight-text)]" : "hover:bg-[var(--highlight-bg)] hover:text-[var(--highlight-text)]"}`}>
+      <div className="group flex flex-col gap-y-2 border-r border-[var(--primary-border)] w-18 h-auto pt-5 px-2 overflow-hidden hover:w-40 transition-all duration-200">
+        <div onClick={() => setActiveTab("upload")} className={`grid grid-cols-[30px_1fr] gap-x-5 px-3 py-3 rounded-xl cursor-pointer transition duration-200 ease-in-out ${activeTab === "upload" ? "bg-[var(--highlight-bg)] text-[var(--highlight-text)]" : "hover:bg-[var(--highlight-bg)] hover:text-[var(--highlight-text)]"}`}>
           <div className="flex justify-center">
             <FontAwesomeIcon icon={faCloudArrowUp} size="xl" />
           </div>
-          <h1 className="text-center opacity-0 group-hover:opacity-100 transition-all duration-200">Upload</h1>
+          <h1 className="opacity-0 group-hover:opacity-100 transition-all duration-200">Upload</h1>
         </div>
-        <div onClick={() => setActiveTab("gallery")} className={`grid grid-cols-[30px_1fr] px-3 py-3 rounded-xl cursor-pointer transition duration-200 ease-in-out ${activeTab === "gallery" ? "bg-[var(--highlight-bg)] text-[var(--highlight-text)]" : "hover:bg-[var(--highlight-bg)] hover:text-[var(--highlight-text)]"}`}>
+        <div onClick={() => setActiveTab("gallery")} className={`grid grid-cols-[30px_1fr] gap-x-5 px-3 py-3 rounded-xl cursor-pointer transition duration-200 ease-in-out ${activeTab === "gallery" ? "bg-[var(--highlight-bg)] text-[var(--highlight-text)]" : "hover:bg-[var(--highlight-bg)] hover:text-[var(--highlight-text)]"}`}>
           <div className="flex justify-center">
             <FontAwesomeIcon icon={faPhotoFilm} size="xl" />
           </div>
-          <h1 className="text-center opacity-0 group-hover:opacity-100 transition-all duration-200">Gallery</h1>
+          <h1 className="opacity-0 group-hover:opacity-100 transition-all duration-200">Gallery</h1>
         </div>
-        <div onClick={() => setActiveTab("settings")} className={`grid grid-cols-[30px_1fr] px-3 py-3 rounded-xl cursor-pointer transition duration-200 ease-in-out ${activeTab === "settings" ? "bg-[var(--highlight-bg)] text-[var(--highlight-text)]" : "hover:bg-[var(--highlight-bg)] hover:text-[var(--highlight-text)]"}`}>
+        <div onClick={() => setActiveTab("settings")} className={`grid grid-cols-[30px_1fr] gap-x-5 px-3 py-3 rounded-xl cursor-pointer transition duration-200 ease-in-out ${activeTab === "settings" ? "bg-[var(--highlight-bg)] text-[var(--highlight-text)]" : "hover:bg-[var(--highlight-bg)] hover:text-[var(--highlight-text)]"}`}>
           <div className="flex justify-center">
             <FontAwesomeIcon icon={faGear} size="xl" />
           </div>
-          <h1 className="text-center opacity-0 group-hover:opacity-100 transition-all duration-200">Settings</h1>
+          <h1 className="opacity-0 group-hover:opacity-100 transition-all duration-200">Settings</h1>
         </div>
       </div>
     </>
