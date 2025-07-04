48f4ece6fd213ce1dc6e25a1343e26a7f6ee84cb
not-sol <castones.sol@gmail.com>
2025-07-05 02:37:02 +0800

    refactor: landing page ui changes

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -3,10 +3,16 @@ import { Link } from 'react-router-dom';
 export const LandingPage = () => {
   return (
     <>
-      <div className="relative grid grid-cols-3 gap-5 py-10 px-40 h-150">
-        <div className="border border-amber-50"></div>
-        <div className="border border-amber-50"></div>
-        <div className="border border-amber-50"></div>
+      <div className="relative flex justify-center gap-2 py-10 h-150">
+        <div className="border rounded-lg overflow-hidden aspect-[9/16]">
+          <img src="/sample.png" className="w-full h-full object-cover brightness-20" />
+        </div>
+        <div className="border rounded-lg overflow-hidden aspect-[9/16]">
+          <img src="/sample.png" className="w-full h-full object-cover brightness-20" />
+        </div>
+        <div className="border rounded-lg overflow-hidden aspect-[9/16]">
+          <img src="/sample.png" className="w-full h-full object-cover brightness-20" />
+        </div>
 
         <div className="absolute inset-0 text-[var(--primary-text)] flex flex-col items-center justify-center">
           <div className="flex justify-center items-center">
@@ -21,13 +27,15 @@ export const LandingPage = () => {
         </div>
       </div>
 
-      <div className="grid grid-cols-[3fr_1fr] gap-x-15 px-70 py-20 text-[var(--primary-text)]">
-        <div className="px-10 py-15 text-2xl">
+      <div className="flex justify-center py-20 h-150 text-[var(--primary-text)]">
+        <div className="flex-5 px-10 py-15 max-w-150 text-2xl">
           <h1 className="text-4xl font-semibold">What is StudySurf?</h1>
           <p className="mt-6">Turn your study documents into visually inducing videos.</p>
           <p className="mt-3">This project allows users to convert their study materials (text, notes, or slides) into engaging video formats using a seamless  stack of modern web and cloud technologies.</p>
         </div>
-        <div className="border border-gray-100 h-full"></div>
+        <div className="rounded-lg overflow-hidden aspect-[9/16]">
+          <img src="/sample.png" className="w-full h-full object-cover brightness-20" />
+        </div>
       </div>
 
       <div className="text-center text-2xl font-semibold py-10 text-[var(--primary-text)]">
