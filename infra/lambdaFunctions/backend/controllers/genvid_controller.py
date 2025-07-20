6434a768b822b72d5ac6f5315c0981deac2e38e9
Carlos Don Lo Priego <lopriegocarl@gmail.com>
2025-07-20 18:34:55 +0800

    Add: "controller for moviepy"

--- infra/lambdaFunctions/backend/controllers/genvid_controller.py
+++ infra/lambdaFunctions/backend/controllers/genvid_controller.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Request, Depends
 from usecases.genvid_usecase import GenVidUseCase
 from services.AWS.bedrock_service import AWS_Bedrock
 from services.AWS.polly_service import AWS_Polly
+from utils.genvid_utils.genvid import MoviePy
 
 
 genvid_router = APIRouter()
@@ -17,8 +18,10 @@ async def generate_video(
     request: Request,
     bedrock: AWS_Bedrock = Depends(AWS_Bedrock),
     polly: AWS_Polly = Depends(AWS_Polly),
+    moviepy: MoviePy = Depends(MoviePy)
+
 ):
     file = await request.body()
 
-    uc = GenVidUseCase(bedrock, polly)
+    uc = GenVidUseCase(bedrock, polly, moviepy)
     return await uc.generate_video_usecase(file)
