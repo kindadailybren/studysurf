27cc110502242e13febc47fe889fe8d6acc3fbab
Jviry <johnvictoryaquino@gmail.com>
2025-07-23 00:59:59 +0800

    feat: grabbing random subway vid from s3 to temp

--- infra/lambdaFunctions/backend/services/AWS/s3_service.py
+++ infra/lambdaFunctions/backend/services/AWS/s3_service.py
@@ -1,5 +1,6 @@
 import boto3
 import os
+import random
 import urllib.parse
 from utils.getFilePath_util import  get_temp_file_path
 
@@ -27,7 +28,6 @@ class AWS_S3:
         print(f"[DEBUG] parsed key: {key}")
         print(f"[DEBUG] local path: {local_path}")
 
-
         try:
             self.s3_client.download_file(self.bucketAudio,key,local_path)
             print(f"[DEBUG] Download successful")
@@ -37,15 +37,25 @@ class AWS_S3:
             return None
     
     def grabVideoSubwayFroms3(self):
-        key = ""
-
-        local_path = get_temp_file_path(os.path.basename(key))
-
-
         try:
-            self.s3_client.download_file(self.bucketSubwayVid, key, local_path)
+            response = self.s3_client.list_objects_v2(Bucket=self.bucketSubwayVid)
 
+            if 'Contents' not in response or not response['Contents']:
+                print("No videos found in the S3 bucket.")
+                return None 
+            
+            objects = response['Contents']
+            random_object = random.choice(objects)
+            key = random_object['Key']
+            
+            local_path = get_temp_file_path(os.path.basename(key))
+
+            self.s3_client.download_file(self.bucketSubwayVid, key, local_path)
+            
+            return local_path
+        
         except Exception as e:
+
             print(f"Error downloading audio from S3: {e}")
             return None
 
