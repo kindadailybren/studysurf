270726f247dfb8080fdbecfc7519d99b339b1d83
Carlos Don Lo Priego <lopriegocarl@gmail.com>
2025-07-23 00:11:59 +0800

    Update genvidtest.py

--- infra/lambdaFunctions/backend/usecases/genvidtest.py
+++ infra/lambdaFunctions/backend/usecases/genvidtest.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     generator = MoviePy()
 
     video_input = {
-        "filename": "input.mp4",  # Make sure this file exists in the output folder
+        "filename": "base.mp4",  # Make sure this file exists in the output folder
         "summary_text": "This is a test summary.\nLine 2 of text.",
         "font_size": 32,
         "font_color": "white",
