6b37efe719059a74b0df1e9b7cedcab1a42fe756
not-sol <castones.sol@gmail.com>
2025-08-13 01:37:20 +0800

    feat: upload page status

--- frontend/src/pages/main-subpages/UploadPage.tsx
+++ frontend/src/pages/main-subpages/UploadPage.tsx
@@ -14,15 +14,26 @@ export const UploadPage = () => {
   const username = useAuthStore((state) => state.username);
 
   const FileDropProps = { file, setFile, setData }
-  const SelectVideoStyleProps = { loading, data }
+  const SelectVideoStyleProps = { data }
   const GenVidButtonProps = { file, setLoading, setData, username }
 
   return (
     <>
-      <div className="flex-1 items-center justify-center border-[var(--primary-border)] w-full h-screen mb-10 p-10">
-        <FileDrop {...FileDropProps} />
-        <SelectVideoStyle {...SelectVideoStyleProps} />
-        <GenVidButton {...GenVidButtonProps} />
+      <div className={`flex-1 ${loading ? "flex" : ""} flex-col items-center justify-center border-[var(--primary-border)] w-full h-screen mb-10 p-10`}>{
+        loading ?
+        <>
+          <h1 className="text-xl">Your video is being generated. It will automatically added to your gallery in approximately 9-10 mins.</h1>
+          <button onClick={() => setLoading(false)} className="border px-5 py-2 mt-2 rounded-lg font-semibold transition-all duration-150 group text-[var(--highlight-text)] hover:bg-[var(--highlight-text)] cursor-pointer hover:text-[var(--secondary-bg)]">
+            Generate New Video
+          </button>
+        </>
+        : 
+        <>
+          <FileDrop {...FileDropProps} />
+          <SelectVideoStyle {...SelectVideoStyleProps} />
+          <GenVidButton {...GenVidButtonProps} />
+        </>
+        }
       </div>
     </>
   )
