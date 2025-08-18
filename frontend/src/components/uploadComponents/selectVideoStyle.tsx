cf6e605c4f30b1c8ec6d62189945228fd68bd422
Sol <castones.sol@gmail.com>
2025-08-18 21:46:03 +0800

    feat: upload page status (#92)
    * feat: upload page status
    
    * refactor: added line breaks
    
    * refactor: added line breaks
    
    * Revert "refactor: added line breaks"
    
    This reverts commit 3323d7b31ad65900ebbf2835699860570bc1c1eb.

--- frontend/src/components/uploadComponents/selectVideoStyle.tsx
+++ frontend/src/components/uploadComponents/selectVideoStyle.tsx
@@ -1,14 +1,13 @@
 interface SelectVideoStyleProps {
-  loading: boolean;
   data: { answer: string }
 }
 
-export const SelectVideoStyle = ({ loading, data }: SelectVideoStyleProps) => {
+export const SelectVideoStyle = ({ data }: SelectVideoStyleProps) => {
   return (
     <>
       <h1 className="text-2xl py-3">Select Video:</h1>
       <div className="flex flex-col justify-center text-center w-full border border-solid h-2/6 rounded-xl">
-        {loading ? <h1>Your video is being generated and will automatically be added to your gallery after</h1> : <h1>Answer: {data.answer}</h1>}
+        <h1>Answer: {data.answer}</h1>
       </div>
     </>
   )
